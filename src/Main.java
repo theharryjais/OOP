9987473999c62c365e3f95465c47006d50a9cae1
theharryjais <83801231+theharryjais@users.noreply.github.com>
2021-12-05 19:59:40 +0545

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//Write a Java program to display the following star pattern ***** ***** ***** ***** *****//
+//c Pyramid Star Pattern//
+public class Main {
+
+    public static void main(String[] args) {
+        int rows = 5, k = 0;
+
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
+    }
+}
