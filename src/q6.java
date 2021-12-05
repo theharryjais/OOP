f4beec166fd7c2f64de7a7252d7d3c4158b613bb
theharryjais <83801231+theharryjais@users.noreply.github.com>
2021-12-05 20:05:12 +0545

    intial commit

---
+++ src/q6.java
@@ -0,0 +1,28 @@
+//Write a Java program that accepts three numbers from the user and prints "increasing"
+// if the numbers are in increasing order,
+// "decreasing" if the numbers are in decreasing order, and "Neither increasing or decreasing order" otherwise.
+import java.util.Scanner;
+public class q6 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        double x = in.nextDouble();
+        System.out.print("Input second number: ");
+        double y = in.nextDouble();
+        System.out.print("Input third number: ");
+        double z = in.nextDouble();
+        if (x < y && y < z)
+        {
+            System.out.println("Increasing order");
+        }
+        else if (x > y && y > z)
+        {
+            System.out.println("Decreasing order");
+        }
+        else
+        {
+            System.out.println("Neither increasing or decreasing order");
+        }
+    }
+}
